7a1286d1128427c8a4cf7a4c8f994777e1e815cf
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-11-01 18:14:07 -0300

    Implementando métodos de busca no repositorio

--- src/main/java/application/domain/repositories/TarefaRepository.java
+++ src/main/java/application/domain/repositories/TarefaRepository.java
@@ -3,6 +3,14 @@ package application.domain.repositories;
 import application.domain.entities.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
+
+    List<Tarefa> findByDescricao(String descricao);
+
+    List<Tarefa> findByTitulo(String titulo);
+
+    List<Tarefa> findByStatus(String status);
 }
