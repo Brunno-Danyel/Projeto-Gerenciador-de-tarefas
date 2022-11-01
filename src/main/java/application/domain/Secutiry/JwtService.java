739f300af6ca65f30eea43f8c4d8ea151191c8c9
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-11-01 18:02:46 -0300

    Implementando configurações de segurança

---
+++ src/main/java/application/domain/Secutiry/JwtService.java
@@ -0,0 +1,58 @@
+package application.domain.Secutiry;
+
+import application.domain.entities.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class JwtService {
+    @Value("${security.jwt.expiracao}")
+    private String expiracao;
+
+    @Value("${security.jwt.chave-assinatura}")
+    private String chaveAssinatura;
+
+    public String gerarToken(Usuario usuario) {
+        long expString = Long.valueOf(expiracao);
+        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
+        Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+        Date data = Date.from(instant);
+
+        return Jwts
+                .builder()
+                .setSubject(usuario.getLogin())
+                .setExpiration(data)
+                .signWith(SignatureAlgorithm.HS512, chaveAssinatura)
+                .compact();
+    }
+
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            Date dateExpiracao = claims.getExpiration();
+            LocalDateTime data = dateExpiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterLogin(String token) throws ExpiredJwtException {
+        return (String) obterClaims(token).getSubject();
+    }
+}
---
+++ src/main/java/application/domain/Secutiry/SecurityConfig.java
@@ -0,0 +1,61 @@
+package application.domain.Secutiry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class SecurityConfig {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter() {
+        return new JwtAuthFilter(jwtService, userService);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/tarefas/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/tarefas").hasAnyRole("ADMIN", "USER")
+                .antMatchers("id/{tarefaId}").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/{tarefaId}/concluir").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.POST, "/usuarios/**").permitAll()
+                .anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
+
+}
