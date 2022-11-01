3011b079b49b147cb30bba38a1d338addad5b0fc
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-11-01 17:38:49 -0300

    Criando entidades

---
+++ src/main/java/application/domain/entities/Tarefa.java
@@ -0,0 +1,50 @@
+package application.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tb_numero_tarefa")
+    private Long id;
+
+    @Column(name = "tb_titulo_tarefa")
+    private String titulo;
+
+    @Column(name = "tb_descricao_tarefa")
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "tb_id_user")
+    // @Column(name = "responsavel_tarefa")
+    private Usuario responsavel;
+
+    @Column(name = "tb_prioridade_tarefa")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Enumerated(EnumType.STRING)
+    private PrioridadeEnum prioridade;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    @Column(name = "tb_data_tarefa")
+    private OffsetDateTime deadline;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tb_status")
+    private StatusTarefa status;
+
+    public void concluirTarefa() {
+
+        setStatus(StatusTarefa.CONCLUIDA);
+    }
+}
---
+++ src/main/java/application/domain/entities/Usuario.java
@@ -0,0 +1,39 @@
+package application.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "${campo.login.obrigatorio}")
+    private String login;
+
+    @NotEmpty(message = "${campo.senha.obrigatorio}")
+    @JsonIgnore
+    private String senha;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "responsavel")
+    private List<Tarefa> tarefa = new ArrayList<>();
+
+    private boolean admin;
+}
