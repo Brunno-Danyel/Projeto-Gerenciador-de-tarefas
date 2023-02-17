e6755d8c5ec56a0dc050a9669cccef3292292390
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-02-17 18:49:56 -0300

    Refatoração dos métodos de busca no repositório das tarefas

--- src/main/java/application/domain/repositories/TarefaRepository.java
+++ src/main/java/application/domain/repositories/TarefaRepository.java
@@ -2,6 +2,7 @@ package application.domain.repositories;
 
 import application.domain.entities.Tarefa;
 import application.domain.entities.Usuario;
+import application.domain.enumeration.StatusTarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,17 +13,13 @@ import java.util.Optional;
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
 
-    @Query(value = "select * from tarefa t join usuario u on t.tb_id_user = u.id  where  t.tb_descricao_tarefa like %:descricao% ", nativeQuery = true)
-    List<Tarefa> findByDescricao(@Param(value = "descricao") String descricao);
+    List<Tarefa> findByDescricao(String descricao);
 
-    @Query(value = "select * from tarefa t join usuario u on t.id_user = u.id  where  t.titulo_tarefa like %:titulo% ", nativeQuery = true)
-    List<Tarefa> findByTitulo(@Param(value = "titulo") String titulo);
+    List<Tarefa> findByTitulo(String titulo);
 
     @Query(value = "select * from tarefa t join usuario u on t.tb_id_user = u.id  where t.tb_status like %:status%", nativeQuery = true)
     List<Tarefa> status(@Param(value = "status") String status);
 
-    @Query(value = "select * from tarefa t join usuario u on t.id_user = u.id  where u.nome like %:nome%", nativeQuery = true)
-    List<Tarefa> name(@Param(value = "nome") String nome);
 
     Optional<List<Tarefa>> findByResponsavel(Usuario responsavel);
 }
