3f04a55ebcef9f6c4080c888760caf9ad86879a0
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-03-06 13:51:59 -0300

    Validando atributos da classe de credenciais

--- src/main/java/application/domain/dto/CredenciaisDTO.java
+++ src/main/java/application/domain/dto/CredenciaisDTO.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CredenciaisDTO {
 
+    @NotEmpty(message = "{campo.login.obrigatorio}")
+    @Email(message = "{campo.login.valido}")
     private String login;
+
+    @NotEmpty(message = "{campo.senha.obrigatorio}")
     private String senha;
 }
