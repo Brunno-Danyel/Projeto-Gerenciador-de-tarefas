166faf0df7d10ea97483ab802272d8a1c48b21b9
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-03-16 14:53:02 -0300

    Ajuste na classe de repositório

--- src/main/java/application/domain/repositories/TarefaRepository.java
+++ src/main/java/application/domain/repositories/TarefaRepository.java
@@ -12,8 +12,10 @@ import java.util.Optional;
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
 
+    @Query(value = "select * from tarefa t join usuario u on t.tb_id_user = u.id  where  t.tb_descricao_tarefa like %:descricao% ", nativeQuery = true)
     Optional<List<Tarefa>> findByDescricao(String descricao);
 
+    @Query(value = "select * from tarefa t join usuario u on t.id_user = u.id  where  t.titulo_tarefa like %:titulo% ", nativeQuery = true)
     Optional<List<Tarefa>> findByTitulo(String titulo);
 
     @Query(value = "select * from tarefa t join usuario u on t.tb_id_user = u.id  where t.tb_status like %:status%", nativeQuery = true)
