cd33cd326ae15fa5b2a4499c77fe1deae1c39f02
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-03-18 13:49:46 -0300

    Refatoração no repositório de tarefas

--- src/main/java/application/domain/repositories/TarefaRepository.java
+++ src/main/java/application/domain/repositories/TarefaRepository.java
@@ -13,10 +13,10 @@ public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
 
     @Query(value = "select * from tarefa t join usuario u on t.tb_id_user = u.id  where  t.tb_descricao_tarefa like %:descricao% ", nativeQuery = true)
-    Optional<List<Tarefa>> findByDescricao(String descricao);
+    List<Tarefa> findByDescricao(String descricao);
 
-    @Query(value = "select * from tarefa t join usuario u on t.id_user = u.id  where  t.titulo_tarefa like %:titulo% ", nativeQuery = true)
-    Optional<List<Tarefa>> findByTitulo(String titulo);
+    @Query(value = "select * from tarefa t join usuario u on t.tb_id_user = u.id  where  t.tb_titulo_tarefa like %:titulo% ", nativeQuery = true)
+    List<Tarefa> findByTitulo(String titulo);
 
     @Query(value = "select * from tarefa t join usuario u on t.tb_id_user = u.id  where t.tb_status like %:status%", nativeQuery = true)
     List<Tarefa> status(@Param(value = "status") String status);
