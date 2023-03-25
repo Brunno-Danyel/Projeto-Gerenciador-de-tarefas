8613e00ce067965f5879652477a5f8f0ccc85565
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-03-25 10:29:20 -0300

    >> Ajustes <<

--- src/main/java/application/domain/exception/ControllerException.java
+++ src/main/java/application/domain/exception/ControllerException.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class ResourceException {
+public class ControllerException {
 
     @ExceptionHandler(TarefaException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
