91700238b0c800bff84ea86238aceea37de7cb27
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-04-05 14:09:06 -0300

    Ajustes na configuração de segurança dos endpoints

--- src/main/java/application/domain/secutiry/SecurityConfig.java
+++ src/main/java/application/domain/secutiry/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.http.HttpServletResponse;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -42,20 +44,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/tarefa").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.POST, "/api/email").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/listar").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/id/{tarefaId}").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/descricao/{descricao}").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/status/{status}").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/filtro").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE,"/{tarefaId}").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/{tarefaId}").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.PUT, "/{tarefaId}/concluir").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.POST, "/api/tarefa").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/api/email/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/api/tarefas/listar").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/id/{tarefaId}").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "descricao/{descricao}").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/api/tarefas/status/{status}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/tarefas/filtro").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/tarefas/responsavel/{responsavel}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/tarefas/remover/{tarefaId}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/{tarefaId}").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/{tarefaId}/concluir").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/api/usuarios/admin/{idUsuario}").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/usuarios/**").permitAll()
                 .anyRequest().authenticated().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+                .and().addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, e) -> {
+                    response.setContentType("application/json");
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write("Token expirado: faça login novamente para obter um novo token!");
+                })
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    response.setContentType("application/json");
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    response.getWriter().write("Acesso negado: Você não tem permissão para acessar esse serviço!");
+                });
         ;
     }
 
