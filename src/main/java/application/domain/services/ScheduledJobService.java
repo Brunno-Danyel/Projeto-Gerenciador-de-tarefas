8b481bcea3ab00cbad7eaf30d2c0bd41e6cab0f7
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-04-10 10:47:35 -0300

    Refatoração na classe de agendamento

--- src/main/java/application/domain/services/ScheduledJobService.java
+++ src/main/java/application/domain/services/ScheduledJobService.java
@@ -1,6 +1,5 @@
 package application.domain.services;
 
-import application.domain.entities.Tarefa;
 import application.domain.enumeration.StatusTarefa;
 import application.domain.repositories.TarefaRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,8 +17,6 @@ public class ScheduledJobService {
 
     private static final String TAREFAS_ATRASO = "0 0 8 1/1 * ?";
 
-    @Autowired
-    private TarefaService tarefaService;
 
     @Autowired
     private TarefaRepository tarefaRepository;
@@ -28,9 +24,9 @@ public class ScheduledJobService {
     @Autowired
     private EmailService emailService;
 
-    @Scheduled(cron = "0 12 14 1/1 * ?")
+    @Scheduled(cron = "0 35 10 1/1 * ?")
     private void converterTarefasAtrasadas() {
-        tarefaService.listarTodasTarefas().stream().map(tarefas -> {
+        tarefaRepository.findAll().stream().map(tarefas -> {
             if (tarefas.getDataPrevistaConclusao() != null && tarefas.getDataPrevistaConclusao().isBefore(LocalDate.now()) && tarefas.getDataConclusao() == null) {
                 tarefas.setStatus(StatusTarefa.ATRASADA);
             }
@@ -40,10 +36,10 @@ public class ScheduledJobService {
         log.info("Converção realizada com sucesso!");
     }
 
-    @Scheduled(cron = "0 34 10 1/1 * ?")
+    @Scheduled(cron = "0 36 10 1/1 * ?")
     private void enviarTarefasEmAtraso() {
-         tarefaService.listarTodasTarefas().stream().map(tarefas -> {
-            if(tarefas.getStatus().equals(StatusTarefa.ATRASADA)){
+        tarefaRepository.findAll().stream().map(tarefas -> {
+            if (tarefas.getStatus().equals(StatusTarefa.ATRASADA)) {
                 try {
                     emailService.envioDeEmailTarefaAtrasada(tarefas);
                 } catch (MessagingException e) {
